65f484a0d38e09066bb464425407ab755a4786e4
basavarajrp <basavarajrp4444@gmail.com>
2022-07-17 13:20:04 +0530

    Added the scope file.

--- scope/scope.js
+++ scope/scope.js
@@ -1,10 +1,3 @@
-# ES6+
-JavaScript ES6+ features.
-
-# var VS let
-Variables declared with var have function scope. This means that they are accessible inside the function/block they are defined in.
-
-```javascript
 var guessMe = 2;
 console.log("guessMe: "+guessMe);// A: guessMe is 2
 ( function() {
@@ -13,4 +6,5 @@ console.log("guessMe: "+guessMe);// A: guessMe is 2
     console.log("guessMe: "+guessMe);// C: guessMe is 5
 } )();
 console.log("guessMe: "+guessMe);// D: guessMe is 2
-```
+
+
